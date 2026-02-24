3ca56128058bc3aa3de8ce3d583c58b2f65ae059
dgTr3 <Edugta31@gmail.com>
2026-02-24 16:08:39 -0600

    pide la cantidad de numeros deseados

--- Ejercicio20/Program.cs
+++ Ejercicio20/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("");
+﻿Console.WriteLine("Cuantos números ingresara?");
+int n = int.Parse(Console.ReadLine());
+int positivos = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Ingrese un número {i+1}: ");
+    int numero = int.Parse(Console.ReadLine());
+   
+}
+
